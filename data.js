eee86b32c822d826e1116775d9682932dd7baa25
Lucas Baron <lucasbaron28@hotmail.com>
2021-03-15 17:33:42 -0300

    Módulos 3, 4 e 5

---
+++ data.js
@@ -0,0 +1,43 @@
+module.exports = [
+    {
+        id: "qtmZSjhVshA",
+        title: "Adonai Pinheiro, Líder Técnico Mobile na Smiles",
+        duration: "57 min",
+        price: "Free"
+    },
+
+    {
+        id: "Ewr2yblF-fA",
+        title: "React: testes, performance, como e quando usar | #PR​​ 28",
+        duration: "35 min",
+        price: "Free"
+    },
+    
+    {
+        id: "p0OhKagYdK8",
+        title: "[NLW#4] Encerramento da Quarta Edição",
+        duration: "60 min",
+        price: "Free"
+    },
+
+    {
+        id: "W-SOgt7O1hc",
+        title: "Node.js: carreira, mercado e dicas na prática | #PR​ 27",
+        duration: "32 min",
+        price: "Free"
+    },
+
+    {
+        id: "SrY8qV6UJ1M",
+        title: "Deploy de apps ReactJS para Google Cloud Storage (part. Guilherme Pellizzetti) | Masterclass #17",
+        duration: "48 min",
+        price: "Free"
+    },
+
+    {
+        id: "1KR-a5_CCQU",
+        title: "Rafael Perozin, Senior Software Engineer em Paris - França | Alumni #13",
+        duration: "25 min",
+        price: "Free"
+    }
+]
